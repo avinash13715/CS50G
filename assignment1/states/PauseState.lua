517ed0c1e64870c6c0dd8eb5397b0901f282e143
Miha Komatar <miha.komatar@gmail.com>
2018-08-09 23:50:43 +0200

    Removed game-breaking code

--- assignment1/states/PauseState.lua
+++ assignment1/states/PauseState.lua
@@ -8,19 +8,6 @@ function PauseState:init()
     sounds['pause']:play()
 end
 
-function PauseState:enter(params)
-    self.score = params.score
-
-    -- set the correct trophy
-    if self.score < 5 then
-        trophy = TROPHY_BRONZE
-    elseif self.score < 10 then
-        trophy = TROPHY_SILVER
-    else
-        trophy = TROPHY_GOLD
-    end
-end
-
 function PauseState:render()
     love.graphics.draw(pausebutton, VIRTUAL_WIDTH/2 - pausebutton:getWidth()/2, VIRTUAL_HEIGHT/2 - pausebutton:getHeight()/2)
 end
